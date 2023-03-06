86e752218b13ab1f5dc81f29e8003dde898be32d
tnielsen2 <trentnielsen84@gmail.com>
2023-03-06 09:39:41 -0500

    Use paginator to do more files at a time

--- s3/find_largest_directories.py
+++ s3/find_largest_directories.py
@@ -26,17 +26,29 @@ try:
     conn = session.client('s3')
     # Create new empty dictionary
     top_level_folders = dict()
-    for key in conn.list_objects(Bucket=bucket_name)['Contents']:
 
-        folder = key['Key'].split('/')[0]
-        print(f'Key {key["Key"]} in folder {folder}. {bytes_to_megabytes(key["Size"])} MB')
+    # List all objects in the bucket with a prefix
+    paginator = conn.get_paginator('list_objects_v2')
+    for result in paginator.paginate(Bucket=bucket_name, Prefix=''):
+        for key in result.get('Contents', []):
+            folder = key['Key'].split('/')[0]
+            print(f'Key {key["Key"]} in folder {folder}. {bytes_to_megabytes(key["Size"])} MB')
 
-        if folder in top_level_folders:
-            top_level_folders[folder] += bytes_to_megabytes(key['Size'])
-        else:
-            top_level_folders[folder] = bytes_to_megabytes(key['Size'])
+            if folder in top_level_folders:
+                top_level_folders[folder] += bytes_to_megabytes(key['Size'])
+            else:
+                top_level_folders[folder] = bytes_to_megabytes(key['Size'])
 
+        for prefix in result.get('CommonPrefixes', []):
+            folder = prefix['Prefix'].split('/')[0]
+            print(f'Folder {prefix["Prefix"]} {folder}')
 
+            if folder in top_level_folders:
+                top_level_folders[folder] += 0
+            else:
+                top_level_folders[folder] = 0
+
+    # Print the folder sizes
     for folder, size in top_level_folders.items():
         print(f'Folder: {folder}, size: {bytes_to_megabytes(size)}')
 except UnauthorizedSSOTokenError:
