8b9453404981ec31d496769bf32dbd6f3b2017a7
tnielsen2 <trentnielsen84@gmail.com>
2023-03-15 09:55:42 -0400

    add working script

--- aws-backup/delete_old_backup_recovery_points.py
+++ aws-backup/delete_old_backup_recovery_points.py
@@ -1,59 +1,62 @@
 import argparse
 import boto3
+import logging
 from datetime import datetime, timedelta
 import pytz
 
+# usage
+# python3 ./aws-backup/delete_old_backup_recovery_points.py --region us-east-2 --profile yam-pxg --vault uat-a-us-east-2-backups-vault --plan uat-a-tier-3-archives
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Delete AWS backup recovery points that are more than 14 days old.')
 parser.add_argument('--region', help='AWS region to use')
 parser.add_argument('--profile', help='AWS profile to use')
+parser.add_argument('--vault', help='Name of the backup vault to query')
+#parser.add_argument('--plan', help='ID of the backup plan. Ex: 12345678-1234-1234-1234-123456789123')
 args = parser.parse_args()
 
-# Initialize AWS clients
-ec2 = boto3.Session(profile_name=args.profile).client('ec2', region_name=args.region)
-backup = boto3.Session(profile_name=args.profile).client('backup', region_name=args.region)
-rds = boto3.Session(profile_name=args.profile).client('rds', region_name=args.region)
+# set up logging to file
+logging.basicConfig(filename='backup-deletion.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
-# Calculate 14 days ago
+# Initialize AWS client
+backup = boto3.Session(profile_name=args.profile).client('backup', region_name=args.region)
+
+# Calculate days_ago
 days_ago = datetime.now(pytz.utc) - timedelta(days=14)
 
-# Open log files
-ami_log_file = open('ami.log', 'w')
-volume_log_file = open('volumes.log', 'w')
-rds_log_file = open('rds.log', 'w')
+# find all recovery points older than days_ago
+response = backup.list_recovery_points_by_backup_vault(
+    BackupVaultName=args.vault,
+    ByCreatedBefore=days_ago
+)
 
-# Loop through all EBS snapshots and print recovery points that are more than 14 days old
-snapshots = ec2.describe_snapshots(OwnerIds=['self'])['Snapshots']
-snapshots = sorted(snapshots, key=lambda x: x['StartTime'])
-for snapshot in snapshots:
-    snapshot_date = snapshot['StartTime'].replace(tzinfo=pytz.utc)
-    if snapshot_date < days_ago:
-        backup_id = snapshot['Description']
-        creation_time = snapshot['StartTime'].strftime('%Y-%m-%d %H:%M:%S %Z')
-        volume_log_file.write(f"Recovery point {backup_id} for EBS snapshot {snapshot['SnapshotId']} created on {creation_time} will be deleted.\n")
-        # Uncomment the following line to delete the EBS snapshot
-        # ec2.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
+recovery_points = response['RecoveryPoints']
+recovery_points_sorted = sorted(recovery_points, key=lambda rp: rp['CompletionDate'], reverse=True)
 
-# Loop through all AMIs and print recovery points that are more than 14 days old
-images = ec2.describe_images(Owners=['self'])['Images']
-images = sorted(images, key=lambda x: datetime.strptime(x['CreationDate'], '%Y-%m-%dT%H:%M:%S.%fZ'))
-for image in images:
-    image_date = datetime.strptime(image['CreationDate'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=pytz.utc)
-    if image_date < days_ago:
-        backup_id = image['ImageId']
-        creation_time = image['CreationDate'].replace('T', ' ').replace('Z', ' UTC')
-        ami_log_file.write(f"Recovery point {backup_id} for AMI {image['ImageId']} created on {creation_time} will be deleted.\n")
-        # Uncomment the following line to deregister the AMI
-        # ec2.deregister_image(ImageId=image['ImageId'])
+# delete the recovery points
+for recovery_point in recovery_points_sorted:
+    backup.delete_recovery_point(
+        BackupVaultName=args.vault,
+        RecoveryPointArn=recovery_point['RecoveryPointArn']
+    )
+    logging.info(f"Deleted recovery point {recovery_point['RecoveryPointArn']}, CompletionDate: {recovery_point['CompletionDate']}")
 
-# Loop through all RDS snapshots and print those that are more than 14 days old
-db_snapshots = rds.describe_db_snapshots(SnapshotType='awsbackup')['DBSnapshots']
-db_snapshots = sorted(db_snapshots, key=lambda x: x['SnapshotCreateTime'])
-for db_snapshot in db_snapshots:
-    snapshot_date = db_snapshot['SnapshotCreateTime'].replace(tzinfo=pytz.utc)
-    if snapshot_date < days_ago:
-        snapshot_id = db_snapshot['DBSnapshotIdentifier']
-        creation_time = db_snapshot['SnapshotCreateTime'].strftime('%Y-%m-%d %H:%M:%S %Z')
-        rds_log_file.write(f"Snapshot {snapshot_id} for RDS instance {db_snapshot['DBInstanceIdentifier']} created on {creation_time} will be deleted.\n")
-        # Uncomment the following line to remove the snapshot
-        # rds.delete_db_snapshot(DBSnapshotIdentifier=snapshot_id)
+# find all snapshots older than days_ago
+#response = backup.list_backup_plan_versions(
+#    BackupPlanId=args.plan
+#)
+
+#if 'BackupPlanVersions' not in response:
+#    print(f"No backup plan versions found for plan {args.plan}")
+#    exit(0)
+#else:
+#    # iterate over all the available versions of the specified backup plan
+#    for version in response['BackupPlanVersions']:
+#        for backup_rule in version['BackupPlan']['Rules']:
+#            if backup_rule['RuleName'] == 'my-snapshot-rule':
+#                snapshot_id = backup_rule['TargetBackupVault']['RegexPattern']
+#                backup.delete_backup(
+#                    BackupVaultName=snapshot_id,
+#                    BackupId=version['BackupPlanArn']
+#                )
+#                logging.info(f"Deleted snapshot {version['BackupPlanArn']} for {snapshot_id}")
